4bf8731d26cd09a76d7167a834b6e5bb58466ebb
songqingbo <1792997269@qq.com>
2018-04-19 16:55:14 +0800

    code10

---
+++ main/code10.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+反转一个单链表。
+
+进阶:
+链表可以迭代或递归地反转。你能否两个都实现一遍？
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	currentNode := head
+	var preNode *ListNode
+	preNode = nil
+	nextNode := currentNode.Next
+	for currentNode != nil && nextNode != nil {
+		currentNode.Next = preNode
+		preNode = currentNode
+		currentNode = nextNode
+		nextNode = currentNode.Next
+	}
+	currentNode.Next = preNode
+	return currentNode
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
