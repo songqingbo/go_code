0271fbf39f1e898642e6e8be867f64ab423250c0
songqingbo <1792997269@qq.com>
2018-04-21 15:05:45 +0800

    code17

---
+++ main/code17.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+
+示例 1:
+给定的树 s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+
+示例 2:
+给定的树 s：
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 false。
+ */
+
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+	if isTreesSame(s, t) {
+		return true
+	}
+	return isSubtree(s.Right, t) || isTreesSame(s.Left, t)
+}
+
+func isTreesSame(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if a.Val != b.Val {
+		return false
+	}
+	return isTreesSame(a.Right, b) && isTreesSame(a.Left, b)
+}
