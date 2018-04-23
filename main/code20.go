bd9358fc4719bd23e4c6bcc99acf0bdcb2757b60
songqingbo <1792997269@qq.com>
2018-04-23 17:03:57 +0800

    code20

---
+++ main/code20.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+ */
+
+//更屌的
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		} else {
+			m[v] = i
+		}
+	}
+	return []int{-1, -1}
+}
+
+func twoSum2(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
